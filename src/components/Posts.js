5c6a12ab6a23664625b939ddf069a134335ed96b
Trevor <trevor2492@gmail.com>
2020-12-02 18:59:34 -0600

    created a react app that pulls a list of posts from https://jsonplaceholder.typicode.com/posts and allows the user to add their own title and post to the top of the list. The app implements react-redux as well.

---
+++ src/components/Posts.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
+
+class Posts extends Component {
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
+
+	componentWillReceiveProps(nextProps){
+		if(nextProps.newPost){
+			this.props.posts.unshift(nextProps.newPost);
+		}
+	}
+
+	render() {
+		const postItems = this.props.posts.map(post => (
+			<div key={post.id}>
+				<h3>Title: {post.title}</h3>
+				<p>Body: {post.body}</p>
+			</div>
+		));
+		
+		return (
+			<div>
+				<h1>Posts</h1>
+				{postItems}
+			</div>
+		)
+	}
+}
+
+Posts.propTypes = {
+	fetchPosts: PropTypes.func.isRequired,
+	posts: PropTypes.array.isRequired,
+	newPost: PropTypes.object
+}
+
+const mapStatetoProps = state => ({
+	posts: state.posts.items,
+	newPost: state.posts.item
+})
+
+export default connect(mapStatetoProps, { fetchPosts })(Posts)
